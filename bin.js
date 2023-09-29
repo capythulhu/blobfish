f9e8ce1ad10029187b2e7fad4b4b5dcb07f1a28c
thzoid <thiagozoid@gmail.com>
2023-09-29 00:29:00 -0300

    upgrade npm version

--- bin.js
+++ bin.js
@@ -12,7 +12,7 @@ import sync from './commands/sync.js';
 // Configure yargs
 const y = yargs()
 y.usage('Usage: $0 <command> [options]')
-y.version("1.3.0")
+y.version("1.3.1")
 
 y.command(init)
 y.command(sync)
