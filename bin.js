e59415a3bcf4c9c70cc36d2fd8bf326470605555
thzoid <thiagozoid@gmail.com>
2023-09-29 00:33:40 -0300

    upgrade version

--- bin.js
+++ bin.js
@@ -12,7 +12,7 @@ import sync from './commands/sync.js';
 // Configure yargs
 const y = yargs()
 y.usage('Usage: $0 <command> [options]')
-y.version("1.3.1")
+y.version("1.3.2")
 
 y.command(init)
 y.command(sync)
